29a4a202577ff4f2c2e8b769e6b04ca5108ebff3
Hardik <hardik.routray@rutgers.edu>
2020-07-03 17:36:13 -0400

    ggPhi generation scripts

---
+++ genproductions/ggPhi/findandreplace.py
@@ -0,0 +1,18 @@
+file="unweighted_events_100000_1p7.lhe"
+fo=open("unweighted_events_6000211_100000events_mass1p7.lhe","w")
+with open(file,"r") as f:
+    for line in f:
+        newline = line
+        spline=line.split()
+        if(len(spline)==13):
+            if(spline[0]=="25" and spline[1]=="1" and spline[4] == "0"): #and spline[2]=="1"):
+#                print(spline)
+                #spline[0] = "6000211"
+                #newline = line[1*(len(spline[0])+1):] + '\n'
+                newline = line.replace("     25", "6000211")
+#                print(newline)
+                
+        fo.write(newline)
+fo.close
+
+
